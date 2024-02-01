9a6e359923c20331cbe8a05dba48cf0aa4a7a7b0
Jaecheol Park <nature9th@gmail.com>
2024-02-01 11:49:35 +0000

    algo: LeetCode 1732

---
+++ leet_code_75/1732_find_the_highest_altitude.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int largestAltitude(List<int> gain) {
+    // 최대 고도
+    int maxAltitude = 0;
+    // 누계 고도
+    int accumulatedAltitude = 0;
+
+    for (var i = 0; i < gain.length; i++) {
+        accumulatedAltitude += gain[i];
+        maxAltitude = max(maxAltitude, accumulatedAltitude);
+    }
+    
+    return maxAltitude;
+  }
+}
